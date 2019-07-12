11f4b09ff41b4781b3474392a14236b002107de0
Admin <Admin@Admin-PC>
2019-07-12 22:47:49 +0530

    first commit uploading all files

---
+++ myyproject/src/myyproject/myyclass.java
@@ -0,0 +1,12 @@
+package myyproject;
+
+public class myyclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("hello world:");
+
+	}
+
+}
