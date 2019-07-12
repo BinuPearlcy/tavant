c7ec7aa76b1384deab7cfa6fa7d26a6e481c5852
Admin <Admin@Admin-PC>
2019-07-12 21:54:57 +0530

    First commit uploading all files

---
+++ switchdemo/src/switchdemo.java
@@ -0,0 +1,40 @@
+
+public class switchdemo {
+	
+	
+	
+	int mark=93;
+	
+	public static void main(String[]args) {
+		char grade = 'A' ;
+		
+		 
+		
+		 
+		
+		
+		switch(grade){
+		case 'A' :
+			
+			
+			System.out.println("the mark is greater than 90 and the grade is A");
+			break;
+			
+		case 'B' :
+			
+			System.out.println("the mark is lesser than 90 and the grade is B");
+	        break;
+		case 'C' :
+			
+			System.out.println("the mark is lesser than 90 and the grade is C");
+	        break; 
+		default:
+			
+			
+		System.out.println("if nothing matches this grade ");}
+		}
+	}
+	
+
+	
+	
---
+++ switchdemo/switchdemo/switchdemo/switchdemotest.java
@@ -0,0 +1,26 @@
+package switchdemo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class switchdemotest {
+
+	@Test
+	void test() {
+		switchdemotest helper=new switchdemotest();
+		Object grade;
+		assertequals("grade B",helper.switchdemotest("	grade A","grade B","grade C"));
+	}
+
+	private void assertequals(String string, Object switchdemotest) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private Object switchdemotest(Object grade, Object grade2, Object grade3) {
+		// TODO Auto-generated method stub
+		return null;
+	}}
+
+	
