b1e17d98ea6094a40d34d52ac4737340a17ea14c
Admin <Admin@Admin-PC>
2019-07-12 22:09:39 +0530

    second commit update all files

---
+++ factorial/factorialtest/factorial/factorialtest.java
@@ -0,0 +1,26 @@
+package factorial;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class factorialtest {
+
+	@Test
+	public void test() {
+		factorialtest helper=new factorialtest();
+		assertequals("5040",helper.factorialOfNumberSeven("number=7,factorial=1"));
+		
+	}
+
+	private void assertequals(String string, Object factorialOfNumberSeven) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private Object factorialOfNumberSeven(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ factorial/src/factorial.java
@@ -0,0 +1,13 @@
+
+public class factorial {
+	static int i;
+	static int fact=1;
+	int number=6;
+	public static void main(String []args)
+	{
+for(i=0;i<=6;++i) {
+	fact=fact*i;
+	System.out.println("factorial of number 6 is "+fact);
+}	
+}
+}
