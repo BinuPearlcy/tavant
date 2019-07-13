5f9833dc8d2a28f6920079ed1e4c427b9cf4ed93
Admin <Admin@Admin-PC>
2019-07-13 08:25:12 +0530

    first commit
    uploading all files

---
+++ fahtocel/src/fahtocel/Scanner.java
@@ -0,0 +1,21 @@
+package fahtocel;
+
+import java.io.InputStream;
+
+public class Scanner {
+
+	public Scanner(InputStream in) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public double nextDouble() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int nextInt() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
